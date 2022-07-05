353bd4b1fb119f95e943afda53129fec9e11ec4e
boykoj <localhost>
2022-07-05 09:59:23 +0200

    test

--- train.py
+++ train.py
@@ -1,28 +1,69 @@
+import logging
+import sys
+
+import mlflow
 import numpy as np
-import tensorflow as tf
- 
-input_path = 'mnist.npz'
-with np.load(input_path, allow_pickle=True) as f:
-    x_train, y_train = f['x_train'], f['y_train']
-    x_test, y_test = f['x_test'], f['y_test']
- 
-x_train, x_test = x_train / 255.0, x_test / 255.0
- 
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10, activation='softmax')
-])
- 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-model.compile(optimizer='adam',
-              loss=loss_fn,
-              metrics=['accuracy'])
- 
-model.fit(x_train, y_train, epochs=5)
- 
-model.evaluate(x_test,  y_test, verbose=2)
- 
-output_path = 'model.h5'
-model.save(output_path)
+import pandas as pd
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.tree import DecisionTreeClassifier, export_text
+
+logging.basicConfig(level=logging.WARN)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    try:
+        data = pd.read_csv("./data/Pilze.csv", sep=";")
+    except Exception as e:
+        logger.exception(
+            "Unable to read training & test CSV. Error: %s", e
+        )
+
+    # prepare data
+    del data["gill-attachment"]
+    del data["ring-number"]
+    del data["veil-color"]
+    del data["veil-type"]
+
+    target = np.unique(data["class"])
+
+    x = data.drop(columns="class")
+    y = data["class"]
+
+    # encode ordinal data
+    enc = OrdinalEncoder()
+    x_transformed = enc.fit_transform(x)
+
+    feature_names = x.columns
+    labels = y.unique()
+
+    # split the dataset
+    train_x, test_x, train_y, test_lab = train_test_split(
+        x_transformed, y, test_size=0.2)
+
+    max_depth = int(sys.argv[1]) if len(sys.argv) > 1 else 10
+
+    with mlflow.start_run() as run:
+        clf = DecisionTreeClassifier(max_depth=max_depth)
+        clf.fit(train_x, train_y)
+        tree_rules = export_text(clf, feature_names=list(feature_names))
+
+        print(f"Decision Tree Classifier (max_depth={max_depth}):")
+
+        mlflow.log_param("max_depth", max_depth)
+
+        # predict class based on test values
+        test_pred_decision_tree = clf.predict(test_x)
+
+        accuracy = metrics.accuracy_score(test_lab, test_pred_decision_tree)
+        mlflow.log_metric("accuracy", accuracy)
+
+        confusion_matrix = metrics.confusion_matrix(
+            test_lab,  test_pred_decision_tree)
+
+        tn, fp, fn, tp = confusion_matrix.ravel()
+        mlflow.log_metric("true_negatives", tn)
+        mlflow.log_metric("false_positives", fp)
+        mlflow.log_metric("false_negatives", fn)
+        mlflow.log_metric("true_positives", tp)
