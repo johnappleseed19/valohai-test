815f11487bf3cc5d1b634e11fa7deed29f6aec6d
boykoj <localhost>
2022-07-05 09:50:20 +0200

    d

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import numpy as np
 import tensorflow as tf
-import valohai
  
 input_path = 'mnist.npz'
 with np.load(input_path, allow_pickle=True) as f:
@@ -18,12 +17,12 @@ model = tf.keras.models.Sequential([
  
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 model.compile(optimizer='adam',
-            loss=loss_fn,
-            metrics=['accuracy'])
+              loss=loss_fn,
+              metrics=['accuracy'])
  
 model.fit(x_train, y_train, epochs=5)
  
 model.evaluate(x_test,  y_test, verbose=2)
  
-output_path = valohai.outputs().path('model.h5')
+output_path = 'model.h5'
 model.save(output_path)
